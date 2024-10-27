6e02b28523b70e099537967b5654f2422d63eb28
Oleksandr <malex307ander@gmail.com>
2024-10-27 17:20:18 +0200

    added pagination to gets all beers request

--- src/Controller/BeerController.php
+++ src/Controller/BeerController.php
@@ -4,14 +4,29 @@ namespace App\Controller;
 
 use App\HTTPResource\Beer\BeerResource;
 use App\Repository\BeerRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class BeerController extends BaseController
 {
     #[Route('/beers', name: 'get_beers', methods: ['GET'])]
-    public function index(BeerRepository $repository): JsonResponse
+    public function index(Request $request, PaginatorInterface $paginator, BeerRepository $repository): JsonResponse
     {
-        return $this->successResponse(BeerResource::collection($repository->findAll()));
+        $pagination = $paginator->paginate(
+            $repository->createQueryBuilder('b')->getQuery(),
+            $request->query->getInt('page', 1),
+            $request->query->getInt('limit', 10)
+        );
+
+        return $this->successResponse([
+            'data' => BeerResource::collection($pagination->getItems()),
+            'pagination' => [
+                'current_page' => $pagination->getCurrentPageNumber(),
+                'total_pages' => $pagination->getPageCount(),
+                'total_items' => $pagination->getTotalItemCount(),
+            ]
+        ]);
     }
 }
